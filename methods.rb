ee4a31838569c5dd50bcd3757ddf571cee0d746c
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-08-10 15:06:39 -0300

    my_select method is done

--- methods.rb
+++ methods.rb
@@ -20,10 +20,17 @@ module Enumerable
     end
   end
   def my_select
-    
+    return self.dup unless block_given?
+    new_array=[]
+    i = 0
+    self.my_each { 
+      new_array << self[i] if yield(self[i]) 
+      i += 1
+    }
+    return new_array
   end
 end
 
  [1,2,3].my_each {|num| puts num}
  [1,2,3].my_each_with_index {|num, idx| puts "num is #{num} at index #{idx}"}
-
+ p [1,2,3,4,5,6].my_select { |num| num.even? }
