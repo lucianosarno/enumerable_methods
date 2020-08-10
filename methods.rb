d27e15f9e266cb75710803c757e5b5a6bbfbb4c0
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-08-10 18:18:00 +0100

    put my_each and my_each_with_index

--- methods.rb
+++ methods.rb
@@ -1,9 +1,32 @@
 module Enumerable
-  def my_each(array)
-    my_size=array.size
-    puts 'size: '+size
-    size.times do
-      yield
-    end
+  
+  def my_each
+   return self.dup unless block_given?
+   
+   i = 0
+
+   while i <= self.length - 1
+    yield(self[i])
+   i += 1
+   end
   end
+
+def  my_each_with_index
+return self.dup unless block_given?
+i = 0
+
+while i <= self.length - 1
+ 
+yield(self[i], i)
+
+i += 1
 end
+
+end
+
+
+end
+
+ [1,2,3].my_each {|num| puts num}
+ [1,2,3].my_each_with_index {|num, idx| puts "num is #{num} at index #{idx}"}
+
