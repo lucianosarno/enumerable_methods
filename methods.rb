0e35972715573e5110e98dc9eb85f2c7666a63b2
AdesojiCodeMaster <adesoji_adewumi@yahoo.com>
2020-08-10 19:36:49 +0100

    fix the methods

--- methods.rb
+++ methods.rb
@@ -1,21 +1,24 @@
 module Enumerable
   
+  # print out individual element
   def my_each
-   return self.dup unless block_given?
+   return self.dup unless block_given? #to prevent block from throwing errors
    
    i = 0
 
-   while i <= self.length - 1
+   while i <= self.length - 1 #while loop
     yield(self[i])
    i += 1
    end
   end
 
+# print out individual element with corresponding index  
 def  my_each_with_index
-return self.dup unless block_given?
+return self.dup unless block_given? #to prevent block from throwing errors
+   
 i = 0
 
-while i <= self.length - 1
+while i <= self.length - 1 #while loop
  
 yield(self[i], i)
 
