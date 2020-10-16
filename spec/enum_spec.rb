0dbb59671e594936fc6a44731f7a506666411d96
Luciano Sarno Soares <63410412+lucianosarno@users.noreply.github.com>
2020-10-16 11:32:43 -0300

    my_each / my_each_with_index rspec done | methods included to methods.rb

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -1,33 +1,23 @@
 require './methods.rb'
 
-# arr = %w(cat dog wombat)
-# hash = Hash.new
-# arr.my_each_with_index { |item, index|
-#   hash[item] = index
-# }
-# puts hash   #=> {"cat"=>0, "dog"=>1, "wombat"=>2}
-
-
-
-
 array_1 = [1, 2, 3]
 describe Enumerable do
-  # describe '#my_each' do
-  #   it '[1, 2, 3].each_cons(3) { |a| a } should return [1, 2, 3]' do
-  #     expect(array_1.my_each { |a| a }).to eql([1, 2, 3])
-  #   end
-  # end
+  describe '#my_each' do
+    it '[1, 2, 3].each_cons(3) { |a| a } should return [1, 2, 3]' do
+      expect(array_1.my_each { |a| a }).to eql([1, 2, 3])
+    end
+  end
 
   describe '#my_each_with_index' do
-    array_2 = ['cat', 'dog', 'wombat']
-    array_2_expected = {"cat"=>0, "dog"=>1, "wombat"=>2}
-    hash_new = Hash.new
-    hash_new = array_2.each_with_index { |item, index|
-      hash_new['a'] = 'b'
-    }  
-    puts "TESTING RUBY-DOCS6 #{hash_new}"
+ 
     it '%w(cat dog wombat).my_each_with_index { |item, index| hash[item] = index } should return {"cat"=>0, "dog"=>1, "wombat"=>2}' do
-      expect(array_2.each_with_index { |item, index| hash_new[item] = index }).to eql(array_2_expected)
+      array_2 = ['cat', 'dog', 'wombat']
+      array_2_expected = {"cat"=>0, "dog"=>1, "wombat"=>2}
+      hash_new = Hash.new
+      array_2.my_each_with_index { |item, index|
+        hash_new[item] = index
+      } 
+      expect(hash_new).to eql(array_2_expected)
     end
   end  
 end
