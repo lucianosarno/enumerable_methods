5e4dba76dc8e243d3b9083f2e510d03b42ae5d06
Gilbert Gotora <gilbertgotora2@gmail.com>
2020-10-16 16:58:58 +0200

    delete commented code

--- spec/enum_spec.rb
+++ spec/enum_spec.rb
@@ -51,12 +51,4 @@ describe Enumerable do
       expect([nil, true, 99].all? ).to eql(false)
     end
   end
-
 end
-
- #=> true
-%w[ant bear cat].all? { |word| word.length >= 4 } #=> false
-%w[ant bear cat].all?(/t/)                        #=> false
-[1, 2i, 3.14].all?(Numeric)                       #=> true
-[nil, true, 99].all?                              #=> false
-[].all?                                           #=> true
